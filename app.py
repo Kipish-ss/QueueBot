84ec3127b90ed910bc590d134adce134ace41543
latand <latand666@gmail.com>
2021-09-10 19:16:11 +0300

    updated requirements.txt

--- app.py
+++ app.py
@@ -16,3 +16,4 @@ async def on_startup(dispatcher):
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
+
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,3 +2,4 @@ from . import errors
 from . import users
 from . import groups
 from . import channels
+
