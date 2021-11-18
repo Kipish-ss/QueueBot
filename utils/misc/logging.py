74e54c77811c59d0a8ffe87e6520666d43c7b284
Kyryl Sidak <ksidak7@gmail.com>
2021-11-19 00:17:20 +0200

    Logging filter added

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,9 +1,22 @@
 import logging
 
+
+class LevelFilter(logging.Filter):
+    def __init__(self, level):
+        self.__level = level
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.levelno <= self.__level
+
+
 file_error_handler = logging.FileHandler('handlers/errors/errors.log')
 file_error_handler.setLevel(logging.ERROR)
 formatter = logging.Formatter(u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s')
 file_error_handler.setFormatter(formatter)
+file_info_handler = logging.FileHandler('info.log')
+file_info_handler.setLevel(logging.INFO)
+file_info_handler.addFilter(LevelFilter(logging.INFO))
+file_info_handler.setFormatter(formatter)
 logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
                     level=logging.INFO,
                     # level=logging.DEBUG,  # Можно заменить на другой уровень логгирования.
