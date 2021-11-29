84bcaaf0aac10e3aa7143062b342cd75549ebb21
Kyryl Sidak <ksidak7@gmail.com>
2021-11-29 22:17:42 +0200

    on_shutdown added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import time
+from data.config import CHAT, TEST_CHAT
 import logging
 from aiogram import executor
 from loader import dp
@@ -7,7 +7,6 @@ from utils.notify import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from utils.misc.logging import file_error_handler
 import asyncio
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
@@ -21,10 +20,16 @@ async def on_startup(dispatcher):
         logger.exception('An unexpected error occured')
 
 
+async def on_shutdown(dispatcher):
+    try:
+        await dp.bot.send_message(CHAT, text='Goodbye!')
+    except Exception as ex:
+        logger.exception(ex)
+
 if __name__ == '__main__':
     try:
         asyncio.get_event_loop().run_until_complete(on_startup_notify(dp))
-        executor.start_polling(dispatcher=dp, on_startup=on_startup, skip_updates=True)
+        executor.start_polling(dispatcher=dp, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=True)
     except TimeoutError as ex:
         logger.exception(ex)
     except Exception:
