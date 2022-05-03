a6264dac9cfbbd81041eeee291fcde9966c03508
Kyryl Sidak <ksidak7@gmail.com>
2022-05-03 12:32:27 +0300

    set_bot_commands.py modified

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -10,12 +10,12 @@ async def set_default_commands(dp):
             types.BotCommand("quit_queue", "Quit queue"),
             types.BotCommand("change_lab", "Change lab number"),
             types.BotCommand("show_number", "Show your number in queue"),
-            types.BotCommand("show_history", "Show count of the people who left the queue."),
+            types.BotCommand("show_history", "Show count of the people who left the queue"),
             types.BotCommand("delete_queue", "Delete current queue"),
             types.BotCommand('show_queue', 'Show queue'),
             types.BotCommand('remove_user', 'Remove user.'),
-            types.BotCommand('next', 'Remove the first person in the queue.'),
-            types.BotCommand('stats', 'Get stats of the queues.'),
+            types.BotCommand('next', 'Remove the first person in the queue'),
+            types.BotCommand('stats', 'Get stats of the queues'),
             types.BotCommand('clear', 'Clear current session')
         ]
     )
