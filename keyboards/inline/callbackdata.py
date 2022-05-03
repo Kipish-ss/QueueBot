b1224afde4055dfcdbd41b5b423a5b154167d9a6
Kyryl Sidak <ksidak7@gmail.com>
2022-05-03 11:48:31 +0300

    delete_queue_callback modified

--- keyboards/inline/callbackdata.py
+++ keyboards/inline/callbackdata.py
@@ -3,5 +3,5 @@ options_callback = CallbackData("choose", "action", "user_id", "user_name", "cha
 lab_callback = CallbackData("priority", "pr_num", "user_id", "user_name", "message_id", "present")
 save_queue_callback = CallbackData("save", "option", "user_id")
 stats_callback = CallbackData("choose", "option", "user_id")
-delete_queue_callback = CallbackData("choose", "option", "user_id")
+delete_queue_callback = CallbackData("delete", "option", "user_id")
 reset_stats_callback = CallbackData("reset", "option", "user_id")
